2be03b6601f9018bd640f87f469a2c92830c1640
Edwino Stein <edwino.stein@gmail.com>
2017-02-22 01:44:11 -0400

    Adicionando barco inimigo

---
+++ Sky War/Assets/Scripts/MoveOn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOn : MonoBehaviour {
+
+	public float speed = 1f;
+	public float baseSpeed = 1000;
+	protected Rigidbody rb;
+
+	void Start () {
+		this.rb = GetComponent<Rigidbody>();
+		this.rb.AddForce (this.transform.forward * this.speed * this.baseSpeed);
+	}
+
+}
